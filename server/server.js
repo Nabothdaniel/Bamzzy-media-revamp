ab63114f67bed25fb80b2315bdbaa92fce94df09
Nabothdaniel <nabothdaniel44@gmail.com>
2025-06-18 09:17:10 +0100

    working

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ app.use('/',(req,res)=>{
 
 // Start server
 sequelize.sync().then(() => {
-  app.listen(PORT,'0.0.0.0' () => {
+  app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
